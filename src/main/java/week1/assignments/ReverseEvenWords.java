1d493302c903b3df88c0948689bb388235f438ca
Swathy <swathynayak@gmail.com>
2019-11-27 17:21:46 +0530

    commiting reverse even words

--- src/main/java/week1/assignments/ReverseEvenWords.java
+++ src/main/java/week1/assignments/ReverseEvenWords.java
@@ -3,9 +3,34 @@ package week1.assignments;
 public class ReverseEvenWords {
 
 	public static void main(String[] args) {
-		// Here is the input
+		// Here +++is the input
 		String test="I am a software tester";
 	
+		String[] splitTest=test.split(" ");
+		// Traverse through each word (using loop)
+		for (int i = 0; i < splitTest.length; i++) {
+			// find the odd index within the loop (use mod operator)
+			if(i%2==1)
+			{
+				//split the words and have it in an array
+				char[] charArray=splitTest[i].toCharArray();
+				for(int j=charArray.length-1;j>=0;j--)
+				{
+					//print the even position words in reverse order using another loop (nested loop)
+					
+					System.out.print(charArray[j]);
+					
+				}
+			}
+			else
+			{
+				// Convert words to character array if the position is even else print the word as it is(concatenate space at the end).
+				System.out.print(" "+splitTest[i]+" ");
+			}
+			
+					
+			}
+		}
 		
 		//Build a logic to reverse the even position words (output: I ma a erawtfos tester)
 		
@@ -22,4 +47,3 @@ public class ReverseEvenWords {
 
 	}
 
-}
